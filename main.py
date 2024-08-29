d61c83e3ac78c5818db5cfa3083956f2b218e7d3
MatejVasky <matej.vasky@gmail.com>
2024-08-29 22:19:02 +0200

    Some changes to main

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from arithmetic_expressions.evaluation import ExpressionEvaluator
 from arithmetic_expressions.functionality_database.exceptions import ParsingError, EvaluationError
 from data_loading import load_functionality
 
+# Welcome message
 print("Welcome to calculator", end="\n\n")
 
+# Functionality loading
 print("Loading functionality...")
 try:
     with open("data/functionality/default.json") as file:
@@ -15,31 +17,42 @@ except:
     quit()
 print("Functionality loaded", end="\n\n")
 
+# Initializing the parser and the evaluator
 parser = ExpressionParser(fd)
 evaluator = ExpressionEvaluator(fd)
 
 while True:
+    # Get input
     expression = input('>> ')
 
+    # Try parsing
     try:
         tokens = parser.parse_expression(expression)
     except ParsingError as e:
         print("An error occurred:", e)
+        continue
+
+    # If the expression is empty, continue
+    if len(tokens) == 0:
+        continue
+
+    # Try evaluating
+    try:
+        res = evaluator.evaluate(tokens)
+    except EvaluationError as e:
+        print("An error occurred:", e)
+        continue
+    
+    # Print result
+    print(f'Result: {res}')
+    
+    # Try getting an approximate value
+    try:
+        approx = res.get_approximate_value()
+    except Exception:
+        print("Failed to compute an approximate value")
     else:
-        if len(tokens) != 0:
-            try:
-                res = evaluator.evaluate(tokens)
-            except EvaluationError as e:
-                print("An error occurred:", e)
-            else:
-                print(f'Result: {res}')
-                
-                try:
-                    approx = res.get_approximate_value()
-                except Exception:
-                    print("Failed to compute an approximate value")
-                else:
-                    if approx != None:
-                        print(f'Approximate result: {approx}')
+        if approx != None:
+            print(f'Approximate result: {approx}')
 
     print()
